472b7e319daf41fdd936145a1b68f0a6d4631151
Brent-Kuhn <bkuhn4669@flpoly.org>
2019-01-17 12:18:55 -0500

    End of class save

--- class1/FirstApp/app/src/main/java/edu/floridapoly/mda/spring19/firstapp/MainActivity.java
+++ class1/FirstApp/app/src/main/java/edu/floridapoly/mda/spring19/firstapp/MainActivity.java
@@ -1,13 +1,19 @@
 package edu.floridapoly.mda.spring19.firstapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloTextView = (TextView) findViewById(R.id.helloText);
     }
 }
