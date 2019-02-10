7435b5a741ff18ab45461c35f5e185357d840a43
Brent Kuhn <bkuhn@Brents-MacBook-Pro.local>
2019-02-10 12:24:19 -0500

    Added new project for individual assignment1

---
+++ NoteTaking_Kuhn/app/src/main/java/kuhn/spring19/cop4656/floridapoly/edu/notes.java
@@ -0,0 +1,13 @@
+package kuhn.spring19.cop4656.floridapoly.edu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
